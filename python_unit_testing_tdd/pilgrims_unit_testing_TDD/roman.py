031b73f1f74a3c442dddcc552987f8e00b736977
Zamzam Fahiye <zamzamft@MacBookPro.lan>
2026-05-15 21:19:39 +0200

    TDD - Green implemented for to_roman

--- python_unit_testing_tdd/pilgrims_unit_testing_TDD/roman.py
+++ python_unit_testing_tdd/pilgrims_unit_testing_TDD/roman.py
@@ -5,12 +5,39 @@
 import unittest
 
 #First iteration - FAIL (TDD - Red)
+"""
 def to_roman(n):
     '''convert integer to Roman numeral'''
     pass                                   
-
+"""
 
 #Second iteration - Pass (TDD - Green)
+roman_numeral_map = (
+    ('M',  1000),
+    ('CM', 900),
+    ('D',  500),
+    ('CD', 400),
+    ('C',  100),
+    ('XC', 90),
+    ('L',  50),
+    ('XL', 40),
+    ('X',  10),
+    ('IX', 9),
+    ('V',  5),
+    ('IV', 4),
+    ('I',  1)
+)                 
+def to_roman(n):
+    '''convert integer to Roman numeral'''
+    result = ''
+    for numeral, integer in roman_numeral_map:
+        while n >= integer:                     
+            result += numeral
+            n -= integer
+    return result
+
+
+#
 
 
 
